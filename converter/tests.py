768a31f2ec99b479b1c6450a537dcedb66805516
Luciana Fujii Pontello <luciana.fujii@collabora.com>
2015-02-02 18:53:09 -0200

    tests: Add test for check_media view

--- converter/tests.py
+++ converter/tests.py
@@ -1,4 +1,14 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 class ConverterTests(TestCase):
+    def test_check_media_without_id(self):
+        data = {}
+        response = self.client.get(reverse('check_media'), data)
+        self.assertContains(response, 'Passe o media_id')
+    def test_check_media_wrong_id(self):
+        data = {'media_id': "22"}
+        response = self.client.get(reverse('check_media'), data)
+        self.assertContains(response, 'incorreto')
+
 

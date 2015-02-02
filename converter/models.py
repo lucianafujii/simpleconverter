d38323c592474e61d6be3a1aa67e12af98cdf998
Luciana Fujii Pontello <luciana.fujii@collabora.com>
2015-02-02 19:29:42 -0200

    Increase size of filename
    Since we are not going to treat filenames at all, at least we should
    accept somewhat big filenames.

--- converter/models.py
+++ converter/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class VideoFile(models.Model):
-    filename = models.CharField(max_length=50)
+    filename = models.CharField(max_length=120)
     media_id = models.PositiveIntegerField(default=0)
     ready = models.BooleanField(default=False)
     error = models.BooleanField(default=False)
